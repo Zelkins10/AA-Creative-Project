8162c9b4aab7db3b9891149fc3653943495ef591
Zelkins10 <mdoyen4@gmail.com>
2021-05-18 00:01:01 +0200

    Clean the code

--- src/sketch.ts
+++ src/sketch.ts
@@ -17,8 +17,6 @@ let luminositeMoyP = 0
 // VISAGES :
 //@ts-ignore ( = ignorer la syntaxe de la ligne suivante)
 const mdlV = new rw.HostedModel({
-    //url: "https://stylegan2-d754f125.hosted-models.runwayml.cloud/v1/", // Lucas
-    //url: "https://stylegan2-79c5c254.hosted-models.runwayml.cloud/v1/", // Maxime
     url: "https://stylegan2-4c8e16cc.hosted-models.runwayml.cloud/v1/", // Maxime (john21)
     
 });
@@ -29,8 +27,6 @@ let frameNbV = 0
 // PAYSAGES :
 //@ts-ignore
 const mdlP = new rw.HostedModel({
-    //url: "https://landscapes-42d075ff.hosted-models.runwayml.cloud/v1/", // Lucas
-    //url: "https://landscapes-21eee1a8.hosted-models.runwayml.cloud/v1/", // Maxime
     url: "https://landscapes-b3d4b050.hosted-models.runwayml.cloud/v1/", // Maxime (john20)
 });
 let imgP: p5.Element
@@ -186,8 +182,6 @@ function make_request_V(){
         // TESTS
         //zV[1]-=1
 
-        //luminositeMoyP = 0 // crée un bug ?
-
         console.log(frameNbV + 1, zV[1], zV[31], zV[141]) // Affichage joie, tristesse, vieillesse
 
         imgV.hide()
@@ -204,157 +198,3 @@ function make_request_V(){
 function windowResized(){
     p6_ResizeCanvas()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//------------------------------ VERSION LUCAS ------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // -------------------
-// //  Parameters and UI
-// // -------------------
-
-// const gui = new dat.GUI()
-// const params = {
-//     Download_Image: () => save(),
-// }
-// gui.add(params, "Download_Image")
-
-// //@ts-ignore
-// //visage :
-// // const mdl = new rw.HostedModel({
-// //     url: "https://stylegan2-d754f125.hosted-models.runwayml.cloud/v1/",
-// // });
-// //@ts-ignore
-// const mdl2 = new rw.HostedModel({
-//     url: "https://landscapes-c8c0cdf9.hosted-models.runwayml.cloud/v1/", // Maxime (john16)
-// });
-
-// let img: p5.Element
-// const z = []
-// let frameNB = 0
-// let img2: p5.Element
-// const z2 = []
-// let frameNB2 = 0
-// const NB_FRAMES_TO_EXPORT = 20
-// let luminositeMoy = 0
-// let i=0
-// // -------------------
-// //       Drawing
-// // -------------------
-
-// function draw() {
-//     if(img){
-//         image(img, 0, 0, width, height);
-//     }
-//     if(img2){
-//         image(img2, 0, 0, width, height);
-//     }
-// }
-
-// // -------------------
-// //    Initialization
-// // -------------------
-
-
-// function setup() {
-//     p6_CreateCanvas()
-
-//     // for (let i = 0; i < 512; i++) {
-//     //     z[i] = random(-0.5, 0.5)
-//     // }
-//     for (let i = 0; i < 512; i++) {
-//         z2[i] = random(-0.5, 0.5)
-//     }
-    
-//     make_request2()
-//     // make_request()
-// }
-// function make_request2() {
-//     const inputs2 = {
-//         "z": z2,
-//         "truncation": 0.8,
-//     };
-//     mdl2.query(inputs2).then(outputs => {
-//         const { image } = outputs
-//         img2 = createImg(image)
-//         for (var x = 0; x < width; x = x + 1) {
-//             for (var y = 0; y < height; y = y + 1) {
-//                 var couleur = get(x, y)
-//                 luminositeMoy = luminositeMoy+((red(couleur)+green(couleur)+blue(couleur))/3)
-//             }
-//         }
-//         img2.hide()
-//         z2[i] += 5 // 0 fait coucher le soleil
-//         //@ts-ignore
-//         p5.prototype.downloadFile(image, frameNB2.toString(), "png")
-//         frameNB2++
-//         i++
-//         if (frameNB2 < NB_FRAMES_TO_EXPORT) {
-//           make_request2()
-//         }
-//     });
-// }
-// // function make_request() {
-// //     const inputs = {
-// //         "z": z,
-// //         "truncation": 0.8,
-// //     };
-// //     mdl.query(inputs).then(outputs => {
-// //         const { image } = outputs
-// //         img = createImg(image)
-// //         // if(i>0){
-// //         //     z[i-1] -= 20
-// //         // }
-// //         // z[i] += 20
-// //         // luminositeMoy = luminositeMoy / (width*height)
-// //         // console.log(luminositeMoy)
-// //         // if (luminositeMoy<128) {
-// //         //     z[1] -= 10 // 0 rend asiatique / 1 rend lumineux et joyeux
-// //         // }
-// //         // else {
-// //         //     z[1] += 10 // 0 rend asiatique / 1 rend lumineux et joyeux
-// //         // }
-// //         // luminositeMoy = 0
-// //         img.hide()
-// //         //@ts-ignore
-// //         p5.prototype.downloadFile(image, frameNB.toString(), "png")
-// //         i++
-// //         frameNB++
-// //         if (frameNB < NB_FRAMES_TO_EXPORT) {
-// //         //   make_request2()
-// //         make_request()
-// //         }
-// //     });
-// // }
-
-// function windowResized() {
-//     p6_ResizeCanvas()
-// }
